2c5f1567607100fa6e1689c9db1244df81a9d8d5
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2023-04-10 21:58:47 -0300

    Implementa métodos do ContainerService

--- PortoApi/Services/Implementacoes/ContainerService.cs
+++ PortoApi/Services/Implementacoes/ContainerService.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PortoApi.DTOs;
 using PortoApi.Models;
 using PortoApi.Services.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PortoApi.Services.Implementacoes
@@ -10,35 +12,84 @@ namespace PortoApi.Services.Implementacoes
     public class ContainerService : IContainerService
     {
         private readonly PortoDBContext _context;
+        private readonly INumeroDeContainerService _numeroDeContainerService;
 
-        public ContainerService(PortoDBContext context)
+        public ContainerService(PortoDBContext context, INumeroDeContainerService numeroDeContainerService)
         {
             _context = context;
+            _numeroDeContainerService = numeroDeContainerService;
         }
 
-        public Task<ActionResult<Container>> AdicionarContainerAsync(ContainerDto containerInput)
+        public async Task<ActionResult<Container>> AdicionarContainerAsync(ContainerDto containerInput)
         {
-            throw new System.NotImplementedException();
+            var numeroDeSerie = await _numeroDeContainerService.CriarNumeroDeSerieAsync();
+            Container container = new Container()
+            {
+                Cliente = containerInput.Cliente,
+                Status = containerInput.Status,
+                Tipo = containerInput.Tipo,
+                Categoria = containerInput.Categoria,
+                NumeroDeSerie = numeroDeSerie,
+            };
+
+            await _context.Containers.AddAsync(container);
+            await _context.SaveChangesAsync();
+
+            return new OkObjectResult(container);
         }
 
-        public Task<ActionResult<Container>> AlterarContainerAsync(string numeroDeSerie, string status, string tipo, string cliente)
+        public async Task<ActionResult<Container>> AlterarContainerAsync(string numeroDeSerie, string? status, string? categoria, string? cliente)
         {
-            throw new System.NotImplementedException();
+            Container? container = await _context.Containers.FirstOrDefaultAsync(c => c.NumeroDeSerie == numeroDeSerie);
+
+            if (container == null)
+                return new BadRequestObjectResult(numeroDeSerie);
+
+            if (status != null)
+                container.Status = status;
+
+            if (categoria != null)
+                container.Categoria = categoria;
+
+            if (cliente != null)
+                container.Cliente = cliente;
+
+            await _context.SaveChangesAsync();
+
+            return new OkObjectResult(container);
         }
 
-        public Task<ActionResult<List<Container>>> ReceberContainerDeClienteAsync(string cliente)
+        public async Task<ActionResult<List<Container>>> ReceberContainerDeClienteAsync(string cliente)
         {
-            throw new System.NotImplementedException();
+            List<Container> containers = await _context.Containers.AsNoTracking().Where(c => c.Cliente == cliente).ToListAsync();
+
+            if (containers == null)
+                return new NoContentResult();
+
+            return new OkObjectResult(containers);
         }
 
-        public Task<ActionResult<Container>> ReceberContainerPorNumeroDeSerieAsync(string numeroDeSerie)
+        public async Task<ActionResult<Container>> ReceberContainerPorNumeroDeSerieAsync(string numeroDeSerie)
         {
-            throw new System.NotImplementedException();
+            Container? container = await _context.Containers.AsNoTracking().FirstOrDefaultAsync(c => c.NumeroDeSerie == numeroDeSerie);
+
+            if (container == null)
+                return new BadRequestObjectResult(numeroDeSerie);
+
+            return new OkObjectResult(container);
         }
 
-        public Task<ActionResult> RemoverContainerAsync(string numeroDeSerie)
+        public async Task<ActionResult> RemoverContainerAsync(string numeroDeSerie)
         {
-            throw new System.NotImplementedException();
+            Container? container = await _context.Containers.FirstOrDefaultAsync(c => c.NumeroDeSerie == numeroDeSerie);
+
+            if (container == null)
+                return new BadRequestObjectResult(numeroDeSerie);
+
+            _context.Containers.Remove(container);
+            await _context.SaveChangesAsync();
+
+            return new AcceptedResult();
         }
     }
 }
--- PortoApi/Services/Interfaces/IContainerService.cs
+++ PortoApi/Services/Interfaces/IContainerService.cs
@@ -9,7 +9,7 @@ namespace PortoApi.Services.Interfaces
     public interface IContainerService
     {
         public Task<ActionResult<Container>> AdicionarContainerAsync(ContainerDto containerInput);
-        public Task<ActionResult<Container>> AlterarContainerAsync(string numeroDeSerie, string? status, string? tipo, string? cliente);
+        public Task<ActionResult<Container>> AlterarContainerAsync(string numeroDeSerie, string? status, string? categoria, string? cliente);
         public Task<ActionResult> RemoverContainerAsync(string numeroDeSerie);
         public Task<ActionResult<Container>> ReceberContainerPorNumeroDeSerieAsync(string numeroDeSerie);
         public Task<ActionResult<List<Container>>> ReceberContainerDeClienteAsync(string cliente);
