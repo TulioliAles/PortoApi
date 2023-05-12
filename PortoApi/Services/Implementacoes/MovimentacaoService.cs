c38c3f966065cb5d870f559a109b663b8f3c0573
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2023-05-11 21:11:11 -0300

    Inicia implementação de MovimentacaoService

--- PortoApi/Services/Implementacoes/MovimentacaoService.cs
+++ PortoApi/Services/Implementacoes/MovimentacaoService.cs
@@ -1,8 +1,73 @@
-﻿using PortoApi.Services.Interfaces;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortoApi.DTOs;
+using PortoApi.Models;
+using PortoApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PortoApi.Services.Implementacoes
 {
     public class MovimentacaoService : IMovimentacaoService
     {
+        private readonly PortoDBContext _context;
+
+        public MovimentacaoService(PortoDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<Movimentacao>> AdicionarMovimentacaoAsync(MovimentacaoDto movimentacaoInput)
+        {
+            Container container = await _context.Containers.AsNoTracking().FirstOrDefaultAsync(c => c.NumeroDeSerie == movimentacaoInput.NumeroDeContainer);
+
+            if (container == null)
+                return new BadRequestObjectResult(movimentacaoInput.NumeroDeContainer);
+
+            Movimentacao movimentacao = new Movimentacao()
+            {
+                NumeroDeContainer = movimentacaoInput.NumeroDeContainer,
+                Tipo = movimentacaoInput.Tipo,
+                Inicio = movimentacaoInput.Inicio,
+                Fim = movimentacaoInput.Fim,
+                Cliente = container.NumeroDeSerie
+            };
+
+            await _context.Movimentacaos.AddAsync(movimentacao);
+            await _context.SaveChangesAsync();
+
+            return new OkObjectResult(movimentacao);
+        }
+
+        public Task<ActionResult<Movimentacao>> AlterarDataEHorarioMovimentacaoAsync(int id, DateTime inicio, DateTime fim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Movimentacao>> AlterarTipoDeMovimentacaoAsync(int id, string tipo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<List<Movimentacao>>> ReceberMovimentacaoPorClienteAsync(string cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<List<Movimentacao>>> ReceberMovimentacaoPorContainerAsync(string numeroDeContainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Movimentacao>> ReceberMovimentacaoPorIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult> RemoverMovimentacaoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- PortoApi/Services/Interfaces/IMovimentacaoService.cs
+++ PortoApi/Services/Interfaces/IMovimentacaoService.cs
@@ -1,6 +1,20 @@
-﻿namespace PortoApi.Services.Interfaces
+﻿using Microsoft.AspNetCore.Mvc;
+using PortoApi.DTOs;
+using PortoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PortoApi.Services.Interfaces
 {
     public interface IMovimentacaoService
     {
+        public Task<ActionResult<Movimentacao>> AdicionarMovimentacaoAsync(MovimentacaoDto movimentacaoInput);
+        public Task<ActionResult<Movimentacao>> AlterarTipoDeMovimentacaoAsync(int id, string tipo);
+        public Task<ActionResult<Movimentacao>> AlterarDataEHorarioMovimentacaoAsync(int id, DateTime inicio, DateTime fim);
+        public Task<ActionResult<Movimentacao>> ReceberMovimentacaoPorIdAsync(int id);
+        public Task<ActionResult<List<Movimentacao>>> ReceberMovimentacaoPorClienteAsync(string cliente);
+        public Task<ActionResult<List<Movimentacao>>> ReceberMovimentacaoPorContainerAsync(string numeroDeContainer);
+        public Task<ActionResult> RemoverMovimentacaoAsync(int id);
     }
 }
